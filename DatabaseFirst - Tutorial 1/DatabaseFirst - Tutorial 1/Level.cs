90512c3284000f04c065cdbe8078a55f88b95c97
Conor McGrath <conormcgrath123@gmail.com>
2017-05-14 12:34:42 +0100

    Adding Database First Section

---
+++ DatabaseFirst - Tutorial 1/DatabaseFirst - Tutorial 1/Level.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseFirst___Tutorial_1
+{
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+}
---
+++ DatabaseFirst - Tutorial 1/DatabaseFirst - Tutorial 1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirst___Tutorial_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Courses through Stored Procedure:
+            var context = new PlutoDBContext();
+            var courses = context.GetCourses();
+
+            foreach(var course in courses)
+            {
+                Console.WriteLine("Title: {0}" , course.Title);
+            }
+
+            Console.WriteLine("---------------------------");
+
+            //Find Author that Begins with M
+            var author = from a in context.Authors
+                         where a.Name.StartsWith("M")
+                         select a;
+
+            //Finding a Specific Author
+            var myAuthor = context.Authors
+                .Where(a => a.Name == "Mosh Hamedani")
+                .FirstOrDefault();
+
+            foreach (var aut in author)
+            {
+                Console.WriteLine("Title: {0}", aut.Name);
+            }
+
+            Console.WriteLine(myAuthor.Name);
+
+            //Working with Enums Tutorial:
+            var course = new Course();
+            course.Level = CourseLevel.Beginner; //Beginner = 1
+
+
+            Console.ReadKey();
+        }
+    }
+}

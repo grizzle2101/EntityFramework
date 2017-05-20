3597dca15b717d7424ec090c7c96f3a65629d43f
Conor McGrath <conormcgrath123@gmail.com>
2017-05-20 17:25:19 +0100

    Adding Data Annotations Section

---
+++ DataAnnotations/DataAnnotations/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnnotations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Overring Code First Conventions
+
+            /*
+             * --- Lecture 1 - Overriding Conventions ---
+             * By Convention, Strings are nullable, so by default our database strings are nullable.
+             * 
+             * Objective 1 - Override this convention using Data Annotation:
+             * Step 1 - Edit the Description Property in the Course Class & add the [Required] data annotation.
+             * Step 2 - Create a migration to reflect this change.
+             * step 3 - Update-Database
+             */
+
+
+            /*
+            * --- Lecture 3 - Data Annotations Demo ---
+            * Step 1 - Create the Domain Objects eg Course, Author, Tags etc.
+            * Step 2 - Create the Context that Implments the DBSets for these Objects.
+            */
+
+            Console.ReadKey();
+        }
+    }
+}

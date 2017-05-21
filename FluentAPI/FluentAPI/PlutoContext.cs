5afee123519a5f03e93220c065aa5e01521ea485
Conor McGrath <conormcgrath123@gmail.com>
2017-05-21 18:15:20 +0100

    Fluent API Starting Point

---
+++ FluentAPI/FluentAPI/PlutoContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace FluentAPI
+{
+    class PlutoContext : DbContext
+    {
+        public PlutoContext()
+            : base("name=PlutoContext")
+        {
+
+        }
+        //DB Sets
+        public virtual DbSet<Author> Authors { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+        public virtual DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ FluentAPI/FluentAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             *  --- Fluent API Demo ---
+             *  Setup:
+             *  -Create Database Pluto_FluentAPI
+             */
+        }
+    }
+}

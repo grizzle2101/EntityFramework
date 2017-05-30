147ed4a00140c7c376b48b58eebed0d6e95f2efb
Conor McGrath <conor.mcgrath123@gmail.com>
2017-05-30 17:38:08 +0100

    Finished Lecture 49 - LINQ Extension Methods

--- Queries/Queries/Program.cs
+++ Queries/Queries/Program.cs
@@ -135,6 +135,159 @@ namespace Queries
                 Console.WriteLine("{0}- {1}", x.AuthorName, x.CourseName);
             }
 
+            /*
+             *  --- Lecture 49 - LINQ Extention Methods ---
+             */
+
+            //Task 1 - Restrictions -  Get All Level 1 Courses
+            var myContext = new PlutoContext();
+
+            var AllLevelOneCourses = myContext.Courses
+                .Where(c => c.Level == 1);
+
+            Console.WriteLine("\n ---All Level One Courses:---");
+            foreach(var course in AllLevelOneCourses)
+            {
+                Console.WriteLine("{0}" , course.Name);
+            }
+
+            //Task 2 - Ordering - Order By Name
+            var OrderedLevelOneCourses = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderByDescending(c => c.Name)
+                .ThenByDescending(c => c.Level); //Multiple Types of Ordering using ThenBy
+
+            Console.WriteLine("\n ---Ordering by Level & Name:---");
+            foreach (var course in OrderedLevelOneCourses)
+            {
+                Console.WriteLine("{0}, {1}", course.Name, course.Level);
+            }
+
+            //Task 3 - Projection
+            /*Say we have HUGE Domain Models, and for performance we only want to use a small subset of that data, projection is
+             * how we do that. The same as Annoymous methods we did for LINQ Syntax.
+             * 
+             * Note:
+             * See we use the the c.Author navigation property to access the Author Name, instead of doing a join like in normal SQL.
+             * 
+             * Change 2:
+             * Rather than using select, which returns a List, we can use SelectMany to Flatten the hieracy of lists so we just have the
+             * property that we wish to access
+            */
+            var tags = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderBy(c => c.Name)
+                //.Select(c=> new { CourseName = c.Name, AuthorName = c.Author.Name}); //Retuning an Annoymous Method
+                //.Select(c => c.Tags);
+                .SelectMany(c => c.Tags);
+
+            //foreach(var c in SmallSampleOfData)
+            //{
+            //    foreach (var tag in c)
+            //        Console.WriteLine(tag.Name);
+            //}
+
+
+            //Change 2: - Using Tag Instead of a List.
+            Console.WriteLine("\n--- Selecting Small SubSet(Tags Only) of Course Data: ---");
+            foreach (var t in tags)
+                Console.WriteLine(t.Name);
+
+            //Task 4 - Set Operators
+            /*
+             * In our Last Task 3, we got back a list of Tags, including a few duplicate C# Tags.
+             * To Fix this we can use the Distinct Keyword, to retrive the DISTINCT records.
+             */
+            var distinctTags = context.Courses
+                .Where(c => c.Level == 1)
+                .OrderByDescending(c => c.Name)
+                .ThenByDescending(c => c.Level)
+                .SelectMany(c => c.Tags)
+                .Distinct();
+
+            Console.WriteLine("\n--- Selecting Distinct Tags Only: ---");
+            foreach (var t in distinctTags)
+                Console.WriteLine(t.Name);
+
+            //Task 5 - Grouping
+            /*
+             * In the previous LINQ Syntax tutorials we demonstrated how grouping is different then with normal SQL.
+             * In LINQ we use GroupBy to break down a list into multiple groups eg courses into levels.
+             */
+
+            var GroupedList = context.Courses
+                .Select(c => new { AuthorName = c.Author.Name, CourseName = c.Name, Level = c.Level})
+                .GroupBy(c => c.Level);
+
+
+            Console.WriteLine("\n--- Grouping By Level: ---");
+            foreach (var group in GroupedList)
+            {
+                Console.WriteLine(group.Key);
+                foreach(var item in group)
+                {
+                    Console.WriteLine("{0} - {1} - {2}" , item.CourseName, item.AuthorName, item.Level);
+                }
+            }
+
+            //Task 6 - Joins
+            /*
+             * 3 Types of Join in LINQ Extenstion Methods
+             * InnerJoin - No Relation in our objects, but we want to associate them.
+             * GroupJoin
+             * CrossJoin
+             */
+
+            var innerJoin = context.Courses.Join(context.Authors, //Join on Authors
+                c => c.AuthorId, //Take AuthorId from Course Table
+                a => a.Id, //Take ID from Artist Table
+                (course, author) => new //Expected Output, a CourseName & AuthorName
+                    {
+                        CourseName = course.Name,
+                        AuthorName = author.Name
+                    });
+
+            Console.WriteLine("\n --- InnerJoin Data from Courses & Authors ---");
+            Console.WriteLine("(Imagine we didn't already have this navigation property)");
+            foreach (var item in innerJoin)
+                Console.WriteLine("{0} - {1}", item.AuthorName , item.CourseName);
+
+            //Group Join
+            /*
+             * Group Join is useful for scenarios when we would use a Left Join in SQL and an Aggregate function.
+             * Eg Author & Courses, give all Authors & Count of their courses
+             */
+
+            var GroupJoin = context.Authors
+                .GroupJoin(context.Courses, a => a.Id, c => c.AuthorId, (author, course) => new
+                {
+                    Author = author,
+                    Courses = course.Count()
+                });
+
+            Console.WriteLine("\n --- Group Join - Author Name & Count from Course Table ---");
+            foreach (var item in GroupJoin)
+                Console.WriteLine("{0} - Count:{1}" , item.Author.Name, item.Courses);
+
+
+            //Cross Join
+            /*
+             * Cross Join returns a list of every possible combination of the data between 2 tables. With extenstion methods
+             * we don't have a method called Cross Join, but we do have SelectMany()
+             */
+
+            var CrossJoin = context.Authors
+                .SelectMany(a => context.Courses, 
+                (author, course) => new
+                {
+                    AuthorName = author.Name,
+                    CourseName = course.Name
+                });
+
+            Console.WriteLine("\n --- Cross Join - Return all Courses & Authors(Some Courses should be shared) ---");
+            foreach (var item in crossJoin)
+                Console.WriteLine("{0}, {1}" ,item.AuthorName, item.CourseName);
+
             Console.ReadKey();
         }
     }

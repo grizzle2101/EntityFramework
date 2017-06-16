e02369d52b84d2abc777837f314472db9beb3739
Conor McGrath <conor.mcgrath123@gmail.com>
2017-06-16 09:30:42 +0100

    Finished Lecture 75 - Repository MVC

--- Lecture 75 - MVC Repository - PlutoWeb/PlutoWeb/PlutoWeb/Controllers/HomeController.cs
+++ Lecture 75 - MVC Repository - PlutoWeb/PlutoWeb/PlutoWeb/Controllers/HomeController.cs
@@ -10,22 +10,40 @@ namespace PlutoWeb.Controllers
     public class HomeController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        PlutoContext context;
 
         public HomeController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
+            context = new PlutoContext();
         }
 
         public ActionResult Index()
         {
+            //Bad Way
+            //var Courses = context.Courses.ToList();
+            //return View(Courses);
+            
+            //Proper Way - Using Unit of Work
             var courses = _unitOfWork.Courses.GetAll();
-
             return View(courses);
         }
 
         [HttpPost]
         public ActionResult AddCourse()
         {
+            ////Bad Way
+            //context.Courses.Add(new Course
+            //{
+            //    Name = "New Course at " + DateTime.Now.ToShortDateString(),
+            //    Description = "Description",
+            //    AuthorId = 1,
+            //    FullPrice = 49,
+            //    Level = 1
+            //});
+            //context.SaveChanges();
+
+            //Proper Way - Using UnitOfWork
             _unitOfWork.Courses.Add(new Course
             {
                 Name = "New Course at " + DateTime.Now.ToShortDateString(),
@@ -36,7 +54,7 @@ namespace PlutoWeb.Controllers
             });
 
             _unitOfWork.Complete();
-
+            
             return RedirectToAction("Index");
         }
 
@@ -46,6 +64,11 @@ namespace PlutoWeb.Controllers
 
             return View();
         }
+        //Dispose no Longer needed as we're using Dependency Framework to Resolve this.
+        //public override void Dispose
+        //{
+
+        //}
 
         public ActionResult Contact()
         {
